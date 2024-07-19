6cda8b652bd891b1a32abd11c4f6ecee0f4d9b3d
CIOCANGEORGE\cioca <ciocan_george97@yahoo.com>
2024-07-19 23:28:09 +0300

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -59,7 +59,6 @@ class HomeController extends Controller
         }
         // Dacă cererea este Ajax, returnează doar datele în format JSON
         $matches = MatchesTable::getByDate($date);
-        var_dump($matches);die();
         foreach ($matches as $key => $match) {
             $matches[$key]['Prediction'] = PredictionsTable::getByMatchId($match['Matches']['id']);
         }
