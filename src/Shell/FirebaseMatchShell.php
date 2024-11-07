b8ad737f24998232d0562d68d7bb0ec582032f91
CIOCANGEORGE\cioca <ciocan_george97@yahoo.com>
2024-11-07 20:10:33 +0200

    tets

--- src/Shell/FirebaseMatchShell.php
+++ src/Shell/FirebaseMatchShell.php
@@ -23,82 +23,35 @@ class FirebaseMatchShell extends Shell
 
 
         $this->database = $firebase->createDatabase();
-        $matches = PredictionsTable::getAllStatsOver50(date("Y-m-d"));
-        foreach ($matches as $match) {
-            $this->adaugaMeci(
-                date("Y-m-d", strtotime($match['matches']["utcDate"])),
-                "fotbal",
-                $match['matches']["id"],
-                [
-                    'nume' => $match['matches']["homeName"],
-                    'poza' => $match['matches']["homeLogo"],
-                ],
-                [
-                    'nume' => $match['matches']["awayName"],
-                    'poza' => $match['matches']["awayLogo"],
-                ],
-                [
-                    'G-2-5' => (int)$match['over2'],
-                    'G-1-5' => (int)$match['over1'],
-                    'G-1H0-5' => (int)$match['over0FirstHalf'],
-                    'gg' => (int)$match['over1'],
-                ]
-            );
-            echo "Datele meciului a fost adăugate!\n";
-            die();
-        }
-        die();
-        $meciuri = [
-            [
-                'ziua' => '2024-11-03',
-                'sport' => 'fotbal',
-                'idMeci' => 'meci1',
-                'gazda' => ['nume' => 'Cagliari', 'poza' => 'URL_poza_Cagliari'],
-                'oaspete' => ['nume' => 'Bologna', 'poza' => 'URL_poza_Bologna'],
-                'predictii' => [
-                    'rezultatFinal' => '1X',
-                    'cornere' => '9.5+',
-                    'goluri' => '2.5+',
-                    'cartonase' => ['red' => '2.5+']
-                ]
-            ],
-            [
-                'ziua' => '2024-11-03',
-                'sport' => 'fotbal',
-                'idMeci' => 'meci2',
-                'gazda' => ['nume' => 'Lecce', 'poza' => 'URL_poza_Lecce'],
-                'oaspete' => ['nume' => 'Verona', 'poza' => 'URL_poza_Verona'],
-                'predictii' => [
-                    'rezultatFinal' => '2',
-                    'cornere' => '7.5+',
-                    'goluri' => '2.5+'
-                ]
-            ],
-            [
-                'ziua' => '2024-11-04',
-                'sport' => 'fotbal',
-                'idMeci' => 'meci3',
-                'gazda' => ['nume' => 'AC Milan', 'poza' => 'URL_poza_AC_Milan'],
-                'oaspete' => ['nume' => 'Napoli', 'poza' => 'URL_poza_Napoli'],
-                'predictii' => [
-                    'rezultatFinal' => '1',
-                    'cornere' => '8.5+',
-                    'goluri' => '3.5+',
-                    'cartonase' => ['yellow' => '2.5+']
-                ]
-            ]
-        ];
-
-        foreach ($meciuri as $meci) {
-            $this->adaugaMeci(
-                $meci['ziua'],
-                $meci['sport'],
-                $meci['idMeci'],
-                $meci['gazda'],
-                $meci['oaspete'],
-                $meci['predictii']
-            );
-            echo "Datele meciului {$meci['idMeci']} au fost adăugate!\n";
+        $date = date("Y-m-d");
+        for ($i = 0; $i < 3; $i++) {
+            if ($i > 0) {
+                $date = date("Y-m-d", strtotime(" +" . $i . +" days"));
+            }
+            $matches = PredictionsTable::getAllStatsOver50($date);
+            foreach ($matches as $match) {
+                $this->adaugaMeci(
+                    date("Y-m-d", strtotime($match['matches']["utcDate"])),
+                    "fotbal",
+                    $match['matches']["id"],
+                    [
+                        'nume' => $match['matches']["homeName"],
+                        'poza' => $match['matches']["homeLogo"],
+                    ],
+                    [
+                        'nume' => $match['matches']["awayName"],
+                        'poza' => $match['matches']["awayLogo"],
+                    ],
+                    [
+                        'G-2-5' => (int)$match['over2'],
+                        'G-1-5' => (int)$match['over1'],
+                        'G-1H0-5' => (int)$match['over0FirstHalf'],
+                        'gg' => (int)$match['over1'],
+                    ]
+                );
+                echo "Datele meciului a fost adăugate!\n";
+                die();
+            }
         }
     }
     private function adaugaMeci($ziua, $sport, $idMeci, $gazda, $oaspete, $predictii)
